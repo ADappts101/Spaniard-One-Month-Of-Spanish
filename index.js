9247b6a0f8bc56611d072dbbbcfa441699b7aa17
ADappts <129835970+BearToRear@users.noreply.github.com>
2023-11-24 18:21:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
             var parser = new DOMParser();
             var xmlDoc = parser.parseFromString(this.responseText, "text/xml");
             var textTag = xmlDoc.getElementsByTagName("text");
+            var chapterTag = xmlDoc.getElementsByTagName("chapter");
             var body = document.body;
 
             function addNewChild(parent, child, id) {
@@ -19,12 +20,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
             class TextElem {
                 constructor() {
-                    this.elem = addNewChild(body, "p", `p${counter2}`);
+                    this.elem = addNewChild(body, "p", `p${counter2++}`);
+                }
+            }
+
+            class ChapterElem {
+                constructor() {
+                    this.elem = addNewChild(body, "div", `chapter${counter2++}`);
                 }
             }
 
             var text = new TextElem();
             text.elem.innerHTML = textTag[counter1].childNodes[0].nodeValue;
+
+            // Process chapterTag
+            for (let i = 0; i < chapterTag.length; i++) {
+                let items = chapterTag[i].getElementsByTagName("item");
+
+                let chapter = new ChapterElem();
+                for (let j = 0; j < items.length; j++) {
+                    let itemName = items[j].getAttribute("Name");
+                    let itemPageNo = items[j].getAttribute("PgNo.");
+                    let itemChNo = chapterTag[i].getAttribute("ChNo.");
+
+                    // Do something with the attributes, for example, create a new paragraph
+                    let itemParagraph = addNewChild(chapter.elem, "p", `item${counter2++}`);
+                    itemParagraph.innerHTML = `${itemChNo}: ${itemName} - Page ${itemPageNo}`;
+                }
+            }
         }
     };
     xhttp.open("GET", "index.xml", true);
