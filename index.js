78b69b24c51f364a55ac924166cfb7c116328579
ADappts <129835970+BearToRear@users.noreply.github.com>
2023-11-24 15:59:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ document.addEventListener("DOMContentLoaded", () => {
             var parser = new DOMParser();
             var xmlDoc = parser.parseFromString(this.responseText, "text/xml");
             var text = xmlDoc.getElementsByTagName("text");
-            document.getElementById("demo").innerHTML = text[0:text.length].childNodes[0].nodeValue;
+            var body = document.body;
+            var html = document.documentElement;
+            
+            function adNeCh(parent, child, id){
+                document.createElement(child);
+                child.setAttribute("id", id);
+                parent.appendChild(child);
+            }
+
+            counter1 = 0;
+            counter2 = 1;
+            var textElem = adNeCh("body", "p", `p${counter2+=1}`);
+            document.getElementById(`p${counter2}`).innerHTML = text[counter1].childNodes[0].nodeValue;
         }
     };
     xhttp.open("GET", "index.xml", true);
