a6bf4294cd5df2f5782928b453f0185746f59ce8
ADappts <129835970+BearToRear@users.noreply.github.com>
2023-11-24 17:16:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var parser = new DOMParser();
             var xmlDoc = parser.parseFromString(this.responseText, "text/xml");
-            var text = xmlDoc.getElementsByTagName("text");
+            var textTag = xmlDoc.getElementsByTagName("text");
             var body = document.body;
-            var html = document.documentElement;
-            
-            function adNeCh(parent, child, id){
-                document.createElement(child);
-                child.setAttribute("id", id);
-                parent.appendChild(child);
+
+            function addNewChild(parent, child, id) {
+                var childElem = document.createElement(child);
+                childElem.setAttribute("id", id);
+                parent.appendChild(childElem);
+                return childElem;
             }
 
-            counter1 = 0;
-            counter2 = 1;
-            class TextElem() {
-                constructor(elem){
-                    this.elem = adNeCh(body, "p", `p${counter1+=1}`);
-                    return this.elem;
+            var counter1 = 0;
+
+            class TextElem {
+                constructor() {
+                    this.elem = addNewChild(body, "p", `p${counter1 += 1}`);
                 }
             }
-            document.getElementById(`p${counter1}`).innerHTML = text[counter1].childNodes[0].nodeValue;
+
+            function newClass(className, varName) {
+                window[varName] = new window[className]();
+            }
+
+            newClass("TextElem", "text");
+            text.elem.innerHTML = textTag[counter1].childNodes[0].nodeValue;
         }
     };
     xhttp.open("GET", "index.xml", true);
     xhttp.send();
 });
-
